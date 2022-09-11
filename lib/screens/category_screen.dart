2cbee9734df5230c64a66c31e883adcae33dfa70
tarekmabdallah91 <tarekmabdallah91@gmail.com>
2022-09-11 13:28:33 +0300

    - init the app

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CategoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meals app"),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
